7b760629d86410e612848e53611e02194a0d3240
ocmykr2 <ocmykr2@sjtu.edu.cn>
2023-02-04 19:49:41 +0800

    Codegen Compeleted

--- src/Codegen/Assembly/ViolentBuilder.java
+++ src/Codegen/Assembly/ViolentBuilder.java
@@ -34,7 +34,7 @@ import static java.lang.StrictMath.max;
 public class ViolentBuilder extends IRVisitor  { // Pass
     public ASMRoot root;
 
-    public int OFFSET = 20;
+    public int OFFSET = 32;
 
     public Module module;
     public HashMap<Function, ASMFunc> FuncRelation = new HashMap<>();
@@ -170,7 +170,7 @@ public class ViolentBuilder extends IRVisitor  { // Pass
         StackSize += 4;
         curBlock.addInst(new Sw(Reg.ra, Reg.sp, new Immediate(0)));
 
-        for(int i = 0; i < 4; ++ i) {
+        for(int i = 0; i < 7; ++ i) {
             curBlock.addInst(new Sw(Reg.getReg("t" + i), Reg.sp, new Immediate(StackSize)));
             StackSize += 4;
         }
@@ -199,7 +199,7 @@ public class ViolentBuilder extends IRVisitor  { // Pass
 
         // restore saved reg
         curBlock.addInst(new Lw(Reg.ra, Reg.sp, new Immediate(0)));
-        for(int i = 0; i < 4; ++ i) {
+        for(int i = 0; i < 7; ++ i) {
             curBlock.addInst(new Lw(Reg.getReg("t" + i), Reg.sp, new Immediate(i * 4 + 4)));
         }
 
